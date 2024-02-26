bda3c929a899ad8f854cb29a6a3d61c789334471
bbuing9 <wogudehowl@kaist.ac.kr>
2024-02-26 21:33:51 +0000

    exp_0226

---
+++ experiment_code/logs/0226/run.sh
@@ -0,0 +1,29 @@
+#CUDA_VISIBLE_DEVICES=0,1 python train_ours.py --init_checkpoint_path pretrained_models/Llama2_7b_sharded \
+#	--model_config_path pretrained_models/Llama2_7b_hf --wrapped_class_name LlamaDecoderLayer \
+#	--data_path datasets/alpaca-train.jsonl --added_tokens 1 \
+#	--act_checkpointing --lr 5e-5 --accumulation_steps 16 --batch_size 4 \
+#	--checkpoint_path ./checkpoints/self_kd0.1_long_mp0.1 --mask_p 0.1 --hack --self_kd 0.1 --wb_name self_kd0.1_mp0.1_long
+
+CUDA_VISIBLE_DEVICES=0,1 python train_kd.py --init_checkpoint_path pretrained_models/Llama2_7b_sharded \
+	--model_config_path pretrained_models/Llama2_7b_hf --wrapped_class_name LlamaDecoderLayer \
+	--data_path datasets/alpaca-train.jsonl --prob_path checkpoints/full_prob --added_tokens 1 \
+	--act_checkpointing --lr 5e-5 --accumulation_steps 16 --batch_size 4 \
+	--checkpoint_path ./checkpoints/prev_kd0.1 --hack --self_kd 0.1  --wb_name prev_kd0.1
+
+CUDA_VISIBLE_DEVICES=0,1 python train_kd.py --init_checkpoint_path pretrained_models/Llama2_7b_sharded \
+	--model_config_path pretrained_models/Llama2_7b_hf --wrapped_class_name LlamaDecoderLayer \
+	--data_path datasets/alpaca-train.jsonl --prob_path checkpoints/full_prob --added_tokens 1 \
+	--act_checkpointing --lr 5e-5 --accumulation_steps 16 --batch_size 4 \
+	--checkpoint_path ./checkpoints/prev_kd0.03 --hack --self_kd 0.03  --wb_name prev_kd0.03
+
+CUDA_VISIBLE_DEVICES=0,1 python train_kd.py --init_checkpoint_path pretrained_models/Llama2_7b_sharded \
+	--model_config_path pretrained_models/Llama2_7b_hf --wrapped_class_name LlamaDecoderLayer \
+	--data_path datasets/alpaca-train.jsonl --prob_path checkpoints/full_prob --added_tokens 1 \
+	--act_checkpointing --lr 5e-5 --accumulation_steps 16 --batch_size 4 \
+	--checkpoint_path ./checkpoints/prev_kd0.3 --hack --self_kd 0.3  --wb_name prev_kd0.3
+
+CUDA_VISIBLE_DEVICES=0,1 python train_kd.py --init_checkpoint_path pretrained_models/Llama2_7b_sharded \
+	--model_config_path pretrained_models/Llama2_7b_hf --wrapped_class_name LlamaDecoderLayer \
+	--data_path datasets/alpaca-train.jsonl --prob_path checkpoints/full_prob --added_tokens 1 \
+	--act_checkpointing --lr 5e-5 --accumulation_steps 16 --batch_size 4 \
+	--checkpoint_path ./checkpoints/prev_kd0.01 --hack --self_kd 0.01  --wb_name prev_kd0.01
---
+++ experiment_code/logs/0226/run2.sh
@@ -0,0 +1,17 @@
+CUDA_VISIBLE_DEVICES=2,3 python train_ours.py --init_checkpoint_path pretrained_models/Llama2_7b_sharded \
+	--model_config_path pretrained_models/Llama2_7b_hf --wrapped_class_name LlamaDecoderLayer \
+	--data_path datasets/alpaca-train.jsonl --added_tokens 1 \
+	--act_checkpointing --lr 5e-5 --accumulation_steps 16 --batch_size 4 \
+	--checkpoint_path ./checkpoints/child_d0.3_long_c_sub --child_d --select sub --reverse_p 0.3 --hack --wb_name child_d0.3_long_c_sub
+
+CUDA_VISIBLE_DEVICES=2,3 python train_ours.py --init_checkpoint_path pretrained_models/Llama2_7b_sharded \
+	--model_config_path pretrained_models/Llama2_7b_hf --wrapped_class_name LlamaDecoderLayer \
+	--data_path datasets/alpaca-train.jsonl --added_tokens 1 \
+	--act_checkpointing --lr 5e-5 --accumulation_steps 16 --batch_size 4 \
+	--checkpoint_path ./checkpoints/child_d0.5_long_c_sub --child_d --select sub --reverse_p 0.5 --hack --wb_name child_d0.5_long_c_sub
+
+CUDA_VISIBLE_DEVICES=2,3 python train_ours.py --init_checkpoint_path pretrained_models/Llama2_7b_sharded \
+	--model_config_path pretrained_models/Llama2_7b_hf --wrapped_class_name LlamaDecoderLayer \
+	--data_path datasets/alpaca-train.jsonl --added_tokens 1 \
+	--act_checkpointing --lr 5e-5 --accumulation_steps 16 --batch_size 4 \
+	--checkpoint_path ./checkpoints/child_d0.1_long_c_sub --child_d --select sub --reverse_p 0.1 --hack --wb_name child_d0.1_long_c_sub
